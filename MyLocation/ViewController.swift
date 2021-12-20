56ecad8bbd9f395b6a1101560508eee6b1204155
Danil <63746799+Maqasta@users.noreply.github.com>
2021-12-20 12:35:36 +0200

    add map

--- MyLocation/ViewController.swift
+++ MyLocation/ViewController.swift
@@ -5,15 +5,51 @@
 //  Created by Данил Фролов on 20.12.2021.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest //battery
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1,
+                                    longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate,
+                                        span: span)
+        
+        mapView.setRegion(region,
+                          animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
 }
 
