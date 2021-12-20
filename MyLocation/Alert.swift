2bc70023456f7976a021cdbd3b1480991338a7de
Danil <63746799+Maqasta@users.noreply.github.com>
2021-12-20 15:19:14 +0200

    add location button

---
+++ MyLocation/Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  MyLocation
+//
+//  Created by Данил Фролов on 20.12.2021.
+//
+
+import UIKit
--- MyLocation/ViewController.swift
+++ MyLocation/ViewController.swift
@@ -5,51 +5,100 @@
 //  Created by Данил Фролов on 20.12.2021.
 //
 
-import CoreLocation
+import CoreLocationUI
 import MapKit
 import UIKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-
-    @IBOutlet var mapView: MKMapView!
+class ViewController: UIViewController {
+    private let mapView = MKMapView()
     
-    let manager = CLLocationManager()
+    private let locationButton: CLLocationButton  = {
+        let button = CLLocationButton()
+        button.icon = .arrowOutline
+        button.cornerRadius = 12
+        return button
+    }()
+    
+    private let addPinButton: UIButton  = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.setTitle("Add Pin", for: .normal)
+        return button
+    }()
+    
+    private let deletePinButton: UIButton  = {
+        let button = UIButton()
+        button.backgroundColor = .systemRed
+        button.layer.cornerRadius = 12
+        button.setTitle("Del Pin", for: .normal)
+        return button
+    }()
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(mapView)
+        mapView.addSubview(locationButton)
+        mapView.addSubview(addPinButton)
+        mapView.addSubview(deletePinButton)
+        
+        locationButton.addTarget(self,
+                                 action: #selector(didTapLocationButton),
+                                 for: .touchUpInside)
+        addPinButton.addTarget(self,
+                                 action: #selector(didTapAddPinButton),
+                                 for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = view.bounds
+        locationButton.frame = CGRect(x: mapView.safeAreaInsets.left + 20,
+                              y: mapView.safeAreaInsets.top + 20,
+                              width: 60,
+                              height: 60)
+        addPinButton.frame = CGRect(x: view.frame.width - 90,
+                                      y: mapView.safeAreaInsets.top + 20,
+                                      width: 70,
+                                      height: 60)
+        deletePinButton.frame = CGRect(x: view.frame.width - 180,
+                                       y: mapView.safeAreaInsets.top + 20,
+                                       width: 70,
+                                       height: 60)
     }
 
+    @objc func didTapLocationButton() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    @objc func didTapAddPinButton() {
+        locationManager.startUpdatingLocation()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        manager.desiredAccuracy = kCLLocationAccuracyBest //battery
-        manager.delegate = self
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
+        setupManager()
     }
     
+    func setupManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //battery
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            manager.stopUpdatingLocation()
-            
-            render(location)
-        }
-    }
-    
-    func render(_ location: CLLocation) {
-        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
-                                                longitude: location.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.1,
-                                    longitudeDelta: 0.1)
-        let region = MKCoordinateRegion(center: coordinate,
-                                        span: span)
-        
-        mapView.setRegion(region,
-                          animated: true)
-        
-        let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        mapView.addAnnotation(pin)
+        guard let location = locations.last?.coordinate  else { return }
+        self.locationManager.stopUpdatingLocation()
+        let region = MKCoordinateRegion(center: location,
+                                        latitudinalMeters: 5000,
+                                        longitudinalMeters: 5000)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
     }
 }
 
